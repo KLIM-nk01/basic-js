9d54df77fd2c1617a0c35b0b47caa2b22ee9c1dd
unknown <KLIM_NIKITA_2001@mail.rugit config --global user.email KLIM_NIKITA_2001@mail.rugit config --global user.email KLIM_NIKITA_2001@mail.ru>
2020-09-30 22:40:02 +0300

    completed task transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,52 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+  
+  let convertedArray = arr.slice(0);
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]){
+   case '--discard-next': 
+      convertedArray[i] = undefined;
+      if(i+1 < convertedArray.length) {
+        convertedArray[i+1] = undefined;
+      }
+      break
+
+    case '--discard-prev': 
+      convertedArray[i]= undefined;
+      if(i-1 > 0) {
+        convertedArray[i-1] =undefined;
+      }
+      break
+
+    case '--double-next' :
+      if(i+1 < convertedArray.length) {
+        convertedArray[i] = convertedArray[i+1];
+      } else {
+        convertedArray[i] = undefined;
+      }
+      break
+
+    case '--double-prev' :
+      if(i-1 > 0) {
+        convertedArray[i] = convertedArray[i-1];
+      } else {
+        convertedArray[i] = undefined;
+      }
+      break
+    } 
+  
+    }
+  
+    for(let i=0;i<convertedArray.length;i++) {
+      if(convertedArray[i] === undefined) {
+        convertedArray.splice(i,1);
+        i--;
+      }
+    }
+  return convertedArray;
 };
