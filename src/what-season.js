2380dd8f24732c12a37452f3a7a71fa626ca161f
unknown <KLIM_NIKITA_2001@mail.rugit config --global user.email KLIM_NIKITA_2001@mail.rugit config --global user.email KLIM_NIKITA_2001@mail.ru>
2020-09-30 14:24:23 +0300

    complited task what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (date === undefined) return 'Unable to determine the time of year!';
+  if (date instanceof Date && !isNaN(date)) {
+
+    const earlySpring = 2;
+    const earlySummer = 5;
+    const earlyAutumn = 8;
+    const earlyWinter = 11;
+
+    if (date.getMonth() >= earlySpring && date.getMonth() < earlySummer) {
+      return 'spring';
+    } else if (date.getMonth() >= earlySummer && date.getMonth() < earlyAutumn) {
+      return 'summer';
+    } else if (date.getMonth() >= earlyAutumn && date.getMonth() < earlyWinter) {
+      return 'autumn';
+    } else return 'winter';
+  } else throw new Error();
+
+
+
 };
